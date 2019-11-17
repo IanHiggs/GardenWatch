0ffd08bae7585ad19aa3bc93ac10ab7d3c8fd8fa
IanHiggs <ian@ihws.co.uk>
2019-11-17 10:11:40 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2018 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
+/* 
+    GardenWatch
+
+    Remote control system to monitor greenhouse objects
+    access, temp, humidity etc
+
+    Main Controller Device : Nucleo-F429Z1
+    processor : ARM M4
+    language : c++
+    mbed Microcontroller Library
+    Copyright (c) 2018 ARM Limited
+    SPDX-License-Identifier: Apache-2.0
+
+*/
 
 #include "mbed.h"
 #include "ThisThread.h"
@@ -11,9 +21,15 @@
 
 DigitalOut led1(LED1);
 
-#define SLEEP_TIME                  10000 // (msec)
+#define SLEEP_TIME                  1000 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
 
+/*
+    class to control all IO on control ports
+
+
+
+ */
 class gpio{
 public:
     void toggle_port(int port_no){
@@ -26,6 +42,12 @@ private:
 
 };
 
+/*
+    class to read and set temperature
+    inside greenhouse
+
+
+ */
 class atd{
 public:
     int get_sample_count(int port_no){
@@ -40,6 +62,13 @@ private:
 
 };
 
+
+/*
+    local display is via a lcd 4x20
+    
+
+
+ */
 class lcd{
 public:
     int write_line(int line, std::string msg){
@@ -54,6 +83,12 @@ private:
 
 };
 
+/*
+    class to control transfers to nodes in Garden
+    transfer is by Digi XBee devices in digimesh packets
+
+
+ */
 class xbee_usart{
 public:
     std::string build_packet(std::string data){
